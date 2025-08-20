aba548cb5c681efac2a30492ef9eb5979653d4af
seron <seron@EVOL>
2025-08-20 11:24:10 -0500

    Add project files.

---
+++ TaskbarMusicWidget/BlurHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace TaskbarMusicWidget
+{
+    public static class BlurHelper
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct AccentPolicy
+        {
+            public int AccentState;
+            public int AccentFlags;
+            public int GradientColor;
+            public int AnimationId;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct WindowCompositionAttributeData
+        {
+            public int Attribute;
+            public IntPtr Data;
+            public int SizeOfData;
+        }
+
+        internal enum AccentState
+        {
+            ACCENT_DISABLED = 0,
+            ACCENT_ENABLE_BLURBEHIND = 3,
+            ACCENT_ENABLE_ACRYLICBLURBEHIND = 4
+        }
+
+        [DllImport("user32.dll")]
+        internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+
+        public static void EnableBlur(Window window)
+        {
+            var windowHelper = new WindowInteropHelper(window);
+
+            var accent = new AccentPolicy();
+            accent.AccentState = (int)AccentState.ACCENT_ENABLE_ACRYLICBLURBEHIND;
+            accent.GradientColor = unchecked((int)0x99000000);
+
+            var accentSize = Marshal.SizeOf(accent);
+            var accentPtr = Marshal.AllocHGlobal(accentSize);
+            Marshal.StructureToPtr(accent, accentPtr, false);
+
+            var data = new WindowCompositionAttributeData
+            {
+                Attribute = 19, // WCA_ACCENT_POLICY
+                SizeOfData = accentSize,
+                Data = accentPtr
+            };
+
+            SetWindowCompositionAttribute(windowHelper.Handle, ref data);
+
+            Marshal.FreeHGlobal(accentPtr);
+        }
+    }
+}
---
+++ TaskbarMusicWidget/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskbarMusicWidget
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += (s, e) => BlurHelper.EnableBlur(this);
+            PositionOnTaskbar();
+        }
+
+        private void PositionOnTaskbar()
+        {
+            // Get screen dimensions
+            double screenWidth = SystemParameters.PrimaryScreenWidth;
+            double screenHeight = SystemParameters.PrimaryScreenHeight;
+            double taskbarHeight = screenHeight - SystemParameters.WorkArea.Height;
+
+            // Stick to bottom-left corner
+            this.Left = 0;
+            this.Top = screenHeight - taskbarHeight - this.Height;
+        }
+    }
+}
